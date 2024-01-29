f6f65324ca58ee4d737773576b1851777c07f39b
abocking <141010280+abocking@users.noreply.github.com>
2024-01-29 10:27:56 +0000

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,41 @@
+﻿using Recap;
+
+namespace MyApp // Note: actual namespace depends on the project name.
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Student[] students = new Student[3];
+
+            for (int i = 0; i < students.Length; i++)
+            {
+                Console.WriteLine($"Please enter the name for student {i+1}: ");
+                string name = Console.ReadLine();
+
+                while (string.IsNullOrEmpty(name))
+                {
+                    Console.WriteLine("Error: name cannot be empty. Please enter a valid name.");
+                    Console.WriteLine($"Please enter the name for student {i + 1}: ");
+                    name = Console.ReadLine();
+                }
+
+                Console.WriteLine($"Please enter the age for student {i + 1}: ");
+                int age;
+
+                while (!int.TryParse(Console.ReadLine(), out age))
+                {
+                    Console.WriteLine("Invalid input. Please enter a valid age");
+                    Console.WriteLine($"Please enter the age for student {i + 1}: ");
+                }
+
+                students[i] = new Student(name, age); 
+            } 
+
+            for (int i = 0; i < students.Length; i++)
+            {
+                students[i].DoLab(5 + (i * 5));
+            }
+        }
+    }
+}
---
+++ Student.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Concurrent;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Recap
+{
+    internal class Student
+    {
+        public string name;
+        public int age;
+
+        public Student(string name, int age)
+        {
+            this.name = name;
+            this.age = age;
+        }
+
+        public void DoLab (int labNo)
+        {
+            if (labNo >= 1 && labNo <= 20)
+            {
+                Console.WriteLine($"{name} is {age} years old and is doing lab {labNo}");
+            }
+            else
+            {
+                Console.WriteLine("Error: Invalid lab number. Please enter a lab number between 1 and 20.");
+            }
+        }
+    }
+}
